4135c400c42d046fd26ccd2790172d908c7bf51d
Noah Clements <noahnoah900@gmail.com>
2017-11-06 10:18:17 -0400

    cleaned up the total price code

--- HomeDepot/src/HomeDepot.java
+++ HomeDepot/src/HomeDepot.java
@@ -107,85 +107,17 @@ class HomeDepot {
 		}
 		System.out.println();
 		
-			System.out.println("\n" + "Nuts: " + "$" + nutPrice); 
-			System.out.println("Bolts: " + "$" + boltPrice); 
-			System.out.println("Washers: " + "$" + washerPrice); 
+			System.out.println("\n" + "Nuts: $" + nutPrice); 
+			System.out.println("Bolts: $" + boltPrice); 
+			System.out.println("Washers: $" + washerPrice); 
 		
-		// SUB TOTAL
-		if (i == 1 && y == 1 && r == 1) { // if i, y, and r ALL equal 1
-			subTotal = (NutsDefault * nuts) + (BoltsDefault * bolts) + (WasherDefault * washers); // calculate subtotal
-																									// with all minimum
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (i == 1 && y == 1) { // if i and y equal 1
-			subTotal = (NutsDefault * nuts) + (BoltsDefault * bolts) + (inputWashers * washers); // calculate subtotal
-																									// with nuts&bolts
-																									// minimum, and
-																									// washer data input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (i == 1 && r == 1) { // if i and r equal 1
-			subTotal = (NutsDefault * nuts) + (inputBolts * bolts) + (WasherDefault * washers); // calculate subtotal
-																								// with nuts&washers
-																								// minimum, and bolts
-																								// data input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (i == 1) { // if i equals 1
-			subTotal = (NutsDefault * nuts) + (inputBolts * bolts) + (inputWashers * washers); // calculate subtotal
-																								// with nuts minimum,
-																								// and washer&bolts data
-																								// input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (y == 1 && r == 1) { // if y and r equal 1
-			subTotal = (inputNuts * nuts) + (BoltsDefault * bolts) + (WasherDefault * washers); // calculate subtotal
-																								// with bolts&washers
-																								// minimum, and nuts
-																								// data input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (y == 1) { // if y equals 1
-			subTotal = (inputNuts * nuts) + (BoltsDefault * bolts) + (inputWashers * washers); // calculate subtotal
-																								// with bolts minimum,
-																								// and nuts&washer data
-																								// input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else if (r == 1) { // if r equals 1
-			subTotal = (inputNuts * nuts) + (inputBolts * bolts) + (WasherDefault * washers); // calculate subtotal with
-																								// washer minimum, and
-																								// nuts&bolts data input
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		} else {
-			subTotal = (inputNuts * nuts) + (inputBolts * bolts) + (inputWashers * washers); // or else it will just
-																								// calculate with all
-																								// data inputs
-			System.out.println("Sub-total: " + "$" + Math.round(subTotal * 100.0) / 100.0); // displays Sub-total
-																							// rounded to 2 decimal
-																							// places
-		}
+		// TOTAL w/ HST
 
-		// TOTAL HST
+			total = (nutPrice + boltPrice + washerPrice) * 1.15;
+			System.out.println("Total: $" + Math.round(total * 100.0) / 100.0); 
+		
 
-		totalHST = subTotal * 0.15; // the totalHST is the subTotal calculated above, multiplied by 0.15
-		System.out.println("Total HST: " + "$" + Math.round(totalHST * 100.0) / 100.0); // displays HST rounded to 2
-																						// decimals
-
-		// TOTAL
-		total = subTotal + totalHST; // declares double total as subTotal plus totalHST
-		System.out.println("Total: " + "$" + Math.round(total * 100.0) / 100.0); // displays total rounded to 2 decimals
-																					// places
-		Thread.sleep(1000); // pauses for a second
-		System.out.println("\n" + "Thank you for shopping at Home Depot!"); // makes new line, and prints string
+		System.out.println("\n" + "Thank you for shopping at Home Depot!");
 
 	}
 }
