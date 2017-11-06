8a89fd8a8798d012bcb9b8a1ddfea75451037ec6
Noah Clements <noahnoah900@gmail.com>
2017-11-06 10:26:46 -0400

    cleaner code

--- HomeDepot/src/HomeDepot.java
+++ HomeDepot/src/HomeDepot.java
@@ -2,35 +2,27 @@ import java.io.*;
 import java.util.Scanner;
 
 class HomeDepot {
-	public static void main(String[] args) throws IOException, InterruptedException { // InterruptedException is there
-																						// for the Thread.sleep
+	public static void main(String[] args) throws IOException, InterruptedException { 
 		Scanner Scanner = new Scanner(System.in);
 
 		double nuts = 0.05; // initializes all the doubles
 		double bolts = 0.05;
 		double washers = 0.02;
-		double subTotal = 0, totalHST = 0, total; // initializes and declares subTotal and totalHST as 0, so no errors
-													// appear
+		double total; 
 		int inputNuts, inputBolts, inputWashers;
 
-		int NutsDefault = 50; // initializing and declaring ints
-		int BoltsDefault = 50;
-		int WasherDefault = 50;
-		String enter = "Please enter quantity for "; // initializing and declaring Strings
-		String minimumWashers = "A minimum of 50 washers must be ordered before your order can be processed.  Please re-enter the minimum amount.";
-		String minimumBolts = "A minimum of 50 bolts must be ordered before your order can be processed.  Please re-enter the minimum amount.";
-		String minimumNuts = "A minimum of 50 nuts must be ordered before your order can be processed.  Please re-enter the minimum amount.";
+		String minString1 = "A minimum of 50 ";
+		String minString2 = " washers must be ordered before your order can be processed.  Please re-enter the minimum amount.";
 
-		int i = 0; // declare the 3 ints default # as 0
+		int i = 0; 
 		int y = 0;
 		int r = 0;
 
 		double nutPrice = 0;
 		double boltPrice = 0;
 		double washerPrice = 0;
-		// asking input for nuts
-		System.out.println(enter + "nuts:"); // asks user to input # of nuts wanted
-
+		
+		System.out.println("Please enter quantity for nuts:"); 
 		inputNuts = Scanner.nextInt();
 
 		if (inputNuts >= 50) {
@@ -38,7 +30,7 @@ class HomeDepot {
 			nutPrice = inputNuts * nuts;
 		} else if (inputNuts < 50) {
 			while (inputNuts < 50 && i != 3) { // 3 tries
-				System.out.println(minimumNuts); // asks to re enter value
+				System.out.println(minString1 + "nuts" + minString2); // asks to re enter value
 				inputNuts = Scanner.nextInt();
 				i++;
 				if (i == 3) {
@@ -54,7 +46,7 @@ class HomeDepot {
 		}
 
 		Thread.sleep(200);
-		System.out.println("\n" + enter + "bolts:");
+		System.out.println("\n" + "Please enter quantity for bolts:");
 		inputBolts = Scanner.nextInt();
 
 		if (inputBolts >= 50) {
@@ -62,7 +54,7 @@ class HomeDepot {
 			boltPrice = inputBolts * bolts;
 		} else if (inputBolts < 50) {
 			while (inputBolts < 50 && y != 3) {
-				System.out.println(minimumBolts);
+				System.out.println(minString1 + "bolts" + minString2);
 				inputBolts = Scanner.nextInt();
 				y++;
 				if (y == 3) {
@@ -77,7 +69,7 @@ class HomeDepot {
 		}
 
 		Thread.sleep(200);
-		System.out.println("\n" + enter + "washers:");
+		System.out.println("\n" + "Please enter quantity for washers:");
 		inputWashers = Scanner.nextInt();
 
 		if (inputWashers >= 50) {
@@ -85,11 +77,11 @@ class HomeDepot {
 			washerPrice = inputWashers * washers;
 		} else if (inputWashers < 50) {
 			while (inputWashers < 50 && r != 3) {
-				System.out.println(minimumWashers);
+				System.out.println(minString1 + "washers" + minString2);
 				inputWashers = Scanner.nextInt();
 				r++;
 				if (r == 3) {
-					System.out.println(WasherDefault + " washers have been requested as the minimum.");
+					System.out.println("50 washers have been requested as the minimum.");
 					washerPrice = 50 * washers;
 				}
 			}
